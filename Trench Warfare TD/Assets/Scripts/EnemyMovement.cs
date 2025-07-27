07c1eea0a68932fb8cc6ac50853c7a47970fdef4
signyboi <cooperhillier919@gmail.com>
2025-07-28 09:03:54 +0930

    Enemy movement
    Added enemy movement

---
+++ Trench Warfare TD/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,41 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("References")]
+        [SerializeField] private Rigidbody2D rb;
+    [Header("Attributes")]
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Transform target;
+    private int PathIndex = 0;
+
+    private void Start()
+    {
+        target = LevelManager.main.path[PathIndex];
+    }
+    private void Update()
+    {
+        if (Vector2.Distance(target.position, transform.position) <= 0.1f)
+        {
+            PathIndex++;
+
+            if (PathIndex == LevelManager.main.path.Length)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            else
+            {
+                target = LevelManager.main.path[PathIndex];
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        Vector2 direction = (target.position - transform.position).normalized;
+
+        rb.linearVelocity = direction * moveSpeed; 
+    }
+}
---
+++ Trench Warfare TD/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager main;
+
+    public Transform startPoint;
+    public Transform[] path;
+
+        private void Awake()
+    {
+        main = this;
+    }
+}
+
