e5735eaa7bce0dbc7935d79c2578aa9ad6eb5217
signyboi <cooperhillier919@gmail.com>
2025-07-29 13:18:11 +0930

    Turrets now shoot
    They shoot now

---
+++ Trench Warfare TD/Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("Attributes")]
+    [SerializeField] private float bulletspeed = 5f;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    private Transform target;
+
+    public void SetTarget(Transform _target)
+    {
+        target = _target;
+    }
+
+        private void FixedUpdate()
+    {
+        if (!target) return;
+                  {
+            
+        }
+        Vector2 direction = (target.position - transform.position).normalized;
+                rb.linearVelocity = direction * bulletspeed;
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+
+}
--- Trench Warfare TD/Assets/Scripts/Turret.cs
+++ Trench Warfare TD/Assets/Scripts/Turret.cs
@@ -1,18 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using Unity.VisualScripting;
+
 public class Turret : MonoBehaviour
 {
-    [Header("Attribute")]
-    [SerializeField] private float targetingRange = 5f;
-
     [Header("References")]
-    [SerializeField] private Transform RotationPoint;
-    [SerializeField] private LayerMask enemyMask;
+    [SerializeField] private Transform turretRotationPoint;
+    [SerializeField] private LayerMask enemyMask; 
+    [SerializeField] private GameObject bulletPrefab; // provides a space to insert a bullet prefab in the editor
+    [SerializeField] private Transform firingPoint; //transform is the position scale and rotation of an object, this is used to figure out where the bullets fire from
 
+    [Header("Attribute")]
+    [SerializeField] private float targetingRange = 20f; // range for detecting enemies
+    [SerializeField] private float rotationSpeed = 200f; // speed at which the turret rotates
+    [SerializeField] private float bps = 1f; //bullets per second
+   
     private Transform target;
-
-    private void Update()
+    private float timeUntilFire;
+    void Update()
     {
         if (target == null)
         {
@@ -22,29 +28,57 @@ public class Turret : MonoBehaviour
 
         RotateTowardsTarget();
 
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
+        else
+        {
+            timeUntilFire += Time.deltaTime;
+
+            if (timeUntilFire >= 1f / bps)
+            {
+                Shoot();
+                timeUntilFire = 0f;
+            }
+        }
     }
 
-    
-
+    private void Shoot()
+    {
+        GameObject bulletObject = Instantiate(bulletPrefab, firingPoint.position, Quaternion.identity);
+        Bullet bulletscript = bulletObject.GetComponent<Bullet>();
+        bulletscript.SetTarget(target);
+          }
     private void FindTarget()
     {
-        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
-     
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)
+        transform.position, 0f, enemyMask);
+
         if (hits.Length > 0)
         {
             target = hits[0].transform;
 
         }
     }
-private void RotateTowardsTarget()
+
+    private bool CheckTargetIsInRange()
     {
-        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg;
-        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-        RotationPoint.rotation = targetRotation;
+        return Vector2.Distance(target.position, transform.position) <= targetingRange;
     }
+
+    private void RotateTowardsTarget()
+    {
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
     private void OnDrawGizmosSelected()
     {
         Handles.color = Color.cyan;
-        Handles.DrawWireDisc(transform.position,transform.forward, targetingRange);
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
     }
+
 }
